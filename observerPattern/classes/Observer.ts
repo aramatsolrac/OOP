41d687648b27882c5af91f660ded0df15b4e5b80
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-11-01 20:23:24 -0600

    Create Observer.ts

---
+++ observerPattern/classes/Observer.ts
@@ -0,0 +1,9 @@
+import Subject from "./Subject";
+
+// Observer provides an update interface for objects that need to be notified of a Subject’s changes of state
+class Observer {
+  public update(subject: Subject): void {
+  }
+}
+
+export default Observer;
